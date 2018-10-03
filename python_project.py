091cda33ffee66a472c0b767e79c2b009aa747f1
Jason Rosenblum <jrosenblum123@gmail.com>
2018-10-02 20:46:35 -0500

    changed documentation in time area

--- python_project.py
+++ python_project.py
@@ -131,7 +131,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration. Additionally, does some cool stuff with the time breakdown"""
+    """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
